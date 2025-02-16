9ec6354007b9693ce55558b0911b871dfbe1eefd
alexqzd <alexqm@icloud.com>
2025-02-16 16:26:14 -0600

    Changed Plugin name

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomebridgeSmartThingsAC';
+export const PLATFORM_NAME = 'HomebridgeSmartThingsACoAuth';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-smartthings-ac';
+export const PLUGIN_NAME = 'homebridge-smartthings-ac-oauth';
