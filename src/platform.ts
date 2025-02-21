a9be80030908eb698c369bb4a4c5e85c4c56492c
alexqzd <alexqm@icloud.com>
2025-02-20 18:50:48 -0600

    Added oAuth support

--- src/platform.ts
+++ src/platform.ts
@@ -1,9 +1,37 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic, UnknownContext } from 'homebridge';
+import * as fs from 'fs';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import { SmartThingsAirConditionerAccessory } from './platformAccessory';
-import { BearerTokenAuthenticator, Device, Component, CapabilityReference, SmartThingsClient } from '@smartthings/core-sdk';
+import { RefreshTokenAuthenticator, RefreshTokenStore, RefreshData, Device, Component, CapabilityReference, SmartThingsClient, AuthData } from '@smartthings/core-sdk';
 import { DeviceAdapter } from './deviceAdapter';
+
+
+// on get refresh data, return the refresh token from the json file, and the secrets from the const variables
+// on put auth data, write the auth data to the json file
+
+// get and put refresh data from/to a json file
+export class jsonTokenStore implements RefreshTokenStore {
+  private readonly path: string;
+  private readonly clientId: string;
+  private readonly clientSecret: string
+  constructor(public readonly log: Logger, path: string, clientId: string, clientSecret: string) {
+    this.path = path;
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+  }
+  async getRefreshData(): Promise<RefreshData> {
+    const data = fs.readFileSync(this.path, 'utf8');
+    const json = JSON.parse(data);
+    const refreshData = { refreshToken: json.refreshToken, clientId: this.clientId, clientSecret: this.clientSecret };
+    this.log.debug('Loaded refresh data:', refreshData);
+    return refreshData;
+  }
+  async putAuthData(data: AuthData): Promise<void> {
+    fs.writeFileSync(this.path, JSON.stringify(data));
+    this.log.debug('Saved auth data:', data);
+  }
+}
 
 
 export class SmartThingsPlatform implements DynamicPlatformPlugin {
@@ -12,17 +40,35 @@ export class SmartThingsPlatform implements DynamicPlatformPlugin {
 
   private readonly accessories: PlatformAccessory[] = [];
   private readonly client: SmartThingsClient;
+  private readonly tokenStore: RefreshTokenStore;
+  private readonly refreshTokenAuthenticator: RefreshTokenAuthenticator;
 
   constructor(
     public readonly log: Logger,
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
-    const token = this.config.token as string;
-    this.client = new SmartThingsClient(new BearerTokenAuthenticator(token));
+    const authToken = this.config.authToken as string;
+    const refreshToken = this.config.refreshToken as string;
+    const clientId = this.config.clientId as string;
+    const clientSecret = this.config.clientSecret as string;
 
-    if (token?.trim()) {
-      this.log.debug('Loading devices with token:', token);
+    const jsonPath = 'refreshToken.json';
+    this.tokenStore = new jsonTokenStore(this.log, jsonPath, clientId, clientSecret);
+
+    if (!fs.existsSync('refreshToken.json')) {
+      this.tokenStore.putAuthData({ authToken: authToken, refreshToken: refreshToken });
+      this.log.info('Token store created at', jsonPath);
+    } else {
+      this.log.info('Token store found at', jsonPath);
+    }
+
+    this.refreshTokenAuthenticator = new RefreshTokenAuthenticator('', this.tokenStore);
+    this.client = new SmartThingsClient(this.refreshTokenAuthenticator);
+
+  
+    if (authToken?.trim() && refreshToken?.trim()) {
+      this.log.debug('Loading devices with auth token:', authToken, 'refresh token:', refreshToken);
 
       this.api.on('didFinishLaunching', () => {
         this.client.devices.list()
@@ -30,7 +76,7 @@ export class SmartThingsPlatform implements DynamicPlatformPlugin {
           .catch(err => log.error('Cannot load devices', err));
       });
     } else {
-      this.log.warn('Please congigure your API token and restart homebridge.');
+      this.log.warn('Please congigure your API auth and refresh tokens and restart homebridge.');
     }
   }
 
